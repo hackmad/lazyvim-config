8da00c7ae9f424ed78d4a650390cd5ae4a66c800
Ahmad Kabani <ahmadkabani@yahoo.com>
2023-03-16 11:03:03 -0400

    cleanup

---
+++ lua/plugins/dart-flutter.lua
@@ -0,0 +1,5 @@
+return {
+  { "dart-lang/dart-vim-plugin" },
+  { "natebosch/vim-lsc" },
+  { "natebosch/vim-lsc-dart" },
+}
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,8 @@
 return {
-  --  { "echasnovski/mini.ai", enabled = false },
-  --  { "echasnovski/mini.comment", enabled = false },
-  --  { "echasnovski/mini.pairs", enabled = false },
+  -- { "echasnovski/mini.ai", enabled = false },
+  -- { "echasnovski/mini.bufremove", enabled = false },
+  -- { "echasnovski/mini.comment", enabled = false },
+  -- { "echasnovski/mini.indentscope", enabled = false },
+  -- { "echasnovski/mini.pairs", enabled = false },
+  -- { "echasnovski/mini.surround", enabled = false },
 }
