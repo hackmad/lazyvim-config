d04e2fbf65ddf001f32c03cb102000c8e71189ff
Ahmad Kabani <ahmadkabani@yahoo.com>
2023-03-21 15:55:08 -0400

    add mason and treesitter configs

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "bash-debug-adapter",
+        "bash-language-server",
+        "clang-format",
+        "clangd",
+        "cmake-language-server",
+        "codelldb",
+        "cpplint",
+        "cpptools",
+        "flake8",
+        "gopls",
+        "json-lsp",
+        "lua-language-server",
+        "pyright",
+        "rust-analyzer",
+        "rustfmt",
+        "shfmt",
+        "stylua",
+        "terraform-ls",
+        "tflint",
+      },
+    },
+  },
+}
---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "cpp",
+        "dart",
+        "go",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "rust",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
